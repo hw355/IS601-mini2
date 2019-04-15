20dd78dc2d849b8658d5a258921e2d52d7dd3484
hw355 <hw355@njit.edu>
2019-04-15 13:16:29 -0400

    Task: Test: 6. Create a unit test to update the name of a user in the database to Steve Smith.

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -18,12 +18,21 @@ class ExampleTest extends TestCase
     {
         $this->assertTrue(true);
     }
-
+/*
     public function testUserInsert()
     {
         $user = factory(User::class)->create();
 
         $this->assertNotEmpty($user);
     }
+*/
+    public function testUserUpdate()
+    {
+        $user = User::find(1);
+        $user->name = 'Steve Smith';
+        $user->save();
+        $this->assertTrue($user->name == 'Steve Smith');
+    }
+
 
 }
